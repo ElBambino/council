4c14ae6efdf2d3cd549d758e744ea60a6d359b28
Roach <archabio@gmail.com>
2018-02-23 14:28:24 +0200

    Add DB seeders

---
+++ database/seeds/SampleDataSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Reply;
+use App\Thread;
+use App\Channel;
+use App\Activity;
+use App\Favorite;
+use App\ThreadSubscription;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class SampleDataSeeder extends Seeder
+{
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        $this->channels();
+        $this->threads();
+        Schema::enableForeignKeyConstraints();
+    }
+
+    protected function channels()
+    {
+        Channel::truncate();
+
+        factory(Channel::class, 10)
+            ->create();
+    }
+
+    protected function threads()
+    {
+        Thread::truncate();
+        Reply::truncate();
+        ThreadSubscription::truncate();
+        Activity::truncate();
+        Favorite::truncate();
+
+        factory(Thread::class, 50)
+            ->create();
+    }
+}
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    public function run()
+    {
+        User::truncate();
+
+        factory(User::class)
+            ->create([
+                'name'  => 'John Doe',
+                'email' => 'johndoe@example.com',
+            ]);
+    }
+}
